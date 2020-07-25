2f47a81816306c1326462a6427890de1364afea9
Almann Goo <almann.goo@gmail.com>
2020-07-25 12:36:58 -0700

    Fixes over optimization of benchmarks.
    * Makes the benchmark more DRY, by using inlined template functions
    * Changes `GENERATE` macro to a `constexpr` of `std::array` for our
    parameters.

--- src/bench_varuint.cpp
+++ src/bench_varuint.cpp
@@ -2,25 +2,33 @@
 
 #include <functional>
 #include <tuple>
+#include <array>
 
 #include "varuint.h"
 
 using std::make_tuple;
 
-template<typename Func>
-inline size_t parse_all(buffer &buf, Func parse)
+template<auto parse>
+inline varuint_result parse_all(buffer &buf)
 {
     uint8_t *curr = buf.data();
     size_t rem = buf.size();
-    size_t count = 0;
+    bool all_ok = true;
+    uint64_t sum = 0;
+    size_t total_len = 0;
+
     while (rem > 0)
     {
         auto [ok, value, read_len] = parse(curr, rem);
         rem -= read_len;
         curr += read_len;
-        count++;
+
+        // make sure the results of parsing are observable to avoid optimization
+        all_ok = all_ok || ok;
+        sum += value;
+        total_len += read_len;
     }
-    return count;
+    return make_tuple(all_ok, sum, total_len);
 }
 
 TEST_CASE("Benchmark VarUInt Parsing", "[!benchmark][varuint]")
@@ -29,22 +37,28 @@ TEST_CASE("Benchmark VarUInt Parsing", "[!benchmark][varuint]")
     constexpr auto seed = 0x2507A7AC9E29907BULL;
     constexpr size_t count = 100000U;
 
-    auto [size_name, max] = GENERATE(
+    constexpr auto sizes = std::array {
             make_tuple("2 Byte ", 16383ULL),
             make_tuple("3 Byte ", 2097151ULL),
             make_tuple("4 Byte ", 268435455ULL),
             make_tuple("8 Byte ", 72057594037927935ULL)
-    );
+    };
+
+    constexpr auto parse_types = std::array {
+            make_tuple("Simple ", parse_all<simple_varuint_parse>),
+            make_tuple("PEXT ", parse_all<pext_varuint_parse>)
+    };
 
     buffer buf = random_varuints(seed, 2097151ULL, count);
 
-    BENCHMARK("Simple " + std::string(size_name) + std::to_string(count))
+    for (auto [size_name, size] : sizes)
     {
-        return parse_all(buf, simple_varuint_parse);
-    };
-
-    BENCHMARK("PEXT " + std::string(size_name) + std::to_string(count))
-    {
-        return parse_all(buf, pext_varuint_parse);
-    };
+        for (auto [parse_name, parse_func] : parse_types)
+        {
+            BENCHMARK(parse_name + std::string(size_name) + std::to_string(count))
+            {
+                return parse_func(buf);
+            };
+        }
+    }
 }
